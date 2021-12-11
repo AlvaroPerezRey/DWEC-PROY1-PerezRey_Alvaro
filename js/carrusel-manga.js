5628414c396af46eb028f454c062af2b795eb85a
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2021-12-11 02:08:27 +0100

    Carrusel, header logeado y listas naime/manga

--- js/carrusel-manga.js
+++ js/carrusel-manga.js
@@ -1,48 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario_contacto").addEventListener('submit', validarFormulario); 
-  });
-  
-function validarFormulario(evento) {
-    evento.preventDefault();
+// Pase el mouse y pare
+// Ajustar un método en lugar de getElementById ()
+function byId(id) {
+    // Si es una ID de cadena, devuelve su objeto DOM
+   return  typeof id==="string"?document.getElementById(id):id;
+}
 
-    var usuario = document.getElementById('nombre').value;
-    if(usuario.length == 0) {
-      alert('No has escrito nada en el usuario');
-      return;
+    var index=2,timer=null,banner=byId("banner").getElementsByTagName("div"),len=banner.length,dots=byId("dots").getElementsByTagName("span");
+// el número de tramo es igual a la imagen div
+
+function slideImg() {
+    // Obtenga el rango de deslizamiento del mouse
+    var main=byId("main");
+    // Desliza el temporizador y deja para continuar
+    main.onmouseover=function () {
+        if(timer){
+            clearInterval(timer);
+        }
     }
-    
-    var apellidos = document.getElementById('apellidos').value;
-    if(apellidos.length == 0) {
-      alert('No has escrito nada en los apellidos');
-      return;
+    main.onmouseout=function () {
+        timer=setInterval(function () {
+            index++;
+            // len = 3, el índice solo puede ser 0, 1, 2
+            if(index>=len){
+                index=0;
+            }
+            // Cambia la imagen, también puedes hacer clic en el botón
+            changeImg();
+        },1000);// Llamado de vez en cuando, el lapso está activo cada tres segundos
     }
+    // Se activa automáticamente en main
+    main.onmouseout();
 
-    var email = document.getElementById('email').value;
-    if(email.length == 0) {
-      alert('No has escrito nada en el email');
-      return;
+    // Haga clic en los puntos para cambiar la imagen e repita todos los eventos de clic de enlace de puntos
+    for(var d=0;d<len;d++){
+        // Agregue un atributo de ID a todos los tramos, el valor es d, como el índice del tramo actual
+        dots[d].id=d;
+
+        dots[d].onclick=function () {
+
+            // Cambia el índice del tramo actual, pero hacer clic es el valor final de 3, porque la función cambia el alcance
+            index=this.id;
+            this.className="active";
+            changeImg();
+        }
     }
+}
 
-    var asunto = document.getElementById('asunto').value;
-    if (asunto.length == 0) {
-      alert('El asunto esta vacio');
-      return;
+function changeImg() {
+   // banner [index] .className = 'slide-active';
+    // Iterar a través de img, dots, clear div y dots shadow
+    // Ocultar todo primero, luego mostrar el actual
+    for(var i=0;i<len;i++){
+        banner[i].style.display='none';
+        dots[i].className="";
     }
+    banner[index].style.display='block';
+    dots[index].className="active";
+}
 
-    var captcha = document.getElementById('captcha').value;
-    if(captcha != suma) {
-      alert('Has escrito incorrectamente el captcha');
 
-    }
-
-    this.submit();
-  }
-
-    
-    var arrayCaracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
-    var valor2 = Math.round(Math.random()*10);
-    var suma = valor1+valor2;
-    var mostrarValor1 = document.getElementById("valor1");
-    var mostrarValor2 = document.getElementById("valor2");
-    mostrarValor1.innerHTML = valor1;
-    mostrarValor2.innerHTML = valor2;
+slideImg();
